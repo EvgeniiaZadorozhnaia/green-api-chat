9b47c106fa6cbaa0c93d337e611b0dc450419627
Evgeniia <eu.skorobogatowa@gmail.com>
2025-01-26 22:18:32 +0500

    chat fixed

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,5 +1,10 @@
 import styles from "./Message.module.css";
 
-export default function Message({ className, message }) {
-  return <div className={`${styles[className]}`}>{message}</div>;
+export default function Message({ className, message, time }) {
+  return (
+    <div className={`${styles[className]}`}>
+      <div>{message}</div>
+      <div className={styles.time}>{time}</div>
+    </div>
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
+
+// Допустим, у вас есть иконка глаза, или вы можете использовать любую библиотеку иконок, например, FontAwesome или Material Icons
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function Home({
   idInstance,
@@ -10,6 +14,9 @@ export default function Home({
   setApiTokenInstance,
 }) {
   const navigate = useNavigate();
+
+  // Состояние для переключения видимости apiTokenInstance
+  const [showApiTokenInstance, setShowApiTokenInstance] = useState(false);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -24,37 +31,50 @@ export default function Home({
     setApiTokenInstance(e.target.value);
   };
 
+  const toggleShowApiTokenInstance = () => {
+    setShowApiTokenInstance((prevState) => !prevState);
+  };
+
   return (
-    <>
-      <div className={styles.home_page}>
-        <h1 className={styles.header}>Добро пожаловать в GREEN CHAT!</h1>
-        <form className={styles.login_form} onSubmit={handleLogin}>
-          <Input
-            placeholder="Введите ваш idInstance"
-            value={idInstance}
-            onChange={handleIdInstance}
-            title="idInstance должен состоять из 10 цифр"
-            required
-          />
+    <div className={styles.home_page}>
+      <h1 className={styles.header}>Добро пожаловать в GREEN CHAT!</h1>
+      <form className={styles.login_form} onSubmit={handleLogin}>
+        <Input
+          placeholder="Введите ваш idInstance"
+          value={idInstance}
+          onChange={handleIdInstance}
+          title="idInstance должен состоять из 10 цифр"
+          type="text"
+          required
+        />
+        <div className={styles.inputWrapper}>
           <Input
             placeholder="Введите ваш apiTokenInstance"
             value={apiTokenInstance}
             onChange={handleApiTokenInstance}
             required
+            type={showApiTokenInstance ? "text" : "password"}
           />
-          <Button type="submit" text="Войти" />
-        </form>
-        <div className={styles.footer}>
-          Нет таких данных?
-          <a
-            href="https://console.green-api.com/instanceList"
-            className={styles.link}
-            target="blank"
+          <button
+            type="button"
+            onClick={toggleShowApiTokenInstance}
+            className={styles.eyeIcon}
           >
-            Получить
-          </a>
+            {showApiTokenInstance ? <FaEyeSlash /> : <FaEye />}
+          </button>
         </div>
+        <Button type="submit" text="Войти" />
+      </form>
+      <div className={styles.footer}>
+        Нет таких данных?
+        <a
+          href="https://console.green-api.com/instanceList"
+          className={styles.link}
+          target="blank"
+        >
+          Получить
+        </a>
       </div>
-    </>
+    </div>
   );
 }
